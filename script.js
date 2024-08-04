f3c656e00e9d1f1672c96456b7e2ce37c831d7fe
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-08-04 18:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+const div = document.createElement("div");
+div.id = "browser-info";
+div.innerText= `You are using ${navigator.appName} version ${navigator.appVersion}`;
+
+const body = document.getElementsByTagName("body")[0];
+body.append(div);
